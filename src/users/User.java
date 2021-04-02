6798439b2031e81bec9134eb4a0afddc87727c2c
WalJT <waljt@pm.me>
2021-04-02 12:28:55 +0100

    Add toString method to export user details

--- src/users/User.java
+++ src/users/User.java
@@ -10,8 +10,7 @@ public class User implements Serializable{
 	 * TODO: Documentation
 	 */
 	private static final long serialVersionUID = 2063583864503068064L;
-	
-	protected int userID; // static NextID?
+
 	protected String emailAddress;
 	protected String firstname;
 	protected String surname;
@@ -20,17 +19,33 @@ public class User implements Serializable{
 	
 	public User() {}
 	
+	/**
+	 * @param emailAddress
+	 * @param firstname
+	 * @param surname
+	 * @param password
+	 * @param address
+	 */
 	public User(String emailAddress, String firstname, String surname, String password, String[] address) {
-		this.userID = 0; //TODO: generate random userID
 		this.emailAddress = emailAddress;
 		this.firstname = firstname;
 		this.surname = surname;
 		this.address = address;
+		this.passwordHash = genreatePasswordHash(password);
 	}
 	
-	protected String genreatePasswordHash() {
+	protected String genreatePasswordHash(String password) {
 		
 		return null;
 	}
 	
+	@Override
+	public String toString() {
+		String userDetails;
+		userDetails = "Details of user (customer or admin) " + this.firstname + " " + this.surname + "\n";
+		userDetails += "	-> Email Address: " + emailAddress;
+		return userDetails;
+	}
+
+
 }
