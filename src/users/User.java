7d20ca192b03ad9ef2158007f524719dd1ac18a0
WalJT <waljt@pm.me>
2021-04-02 08:04:13 +0100

    Change capitalization of firstname and surname variables

--- src/users/User.java
+++ src/users/User.java
@@ -13,18 +13,18 @@ public class User implements Serializable{
 	
 	protected int userID; // static NextID?
 	protected String emailAddress;
-	protected String firstName;
-	protected String surName;
+	protected String firstname;
+	protected String surname;
 	protected String passwordHash;
 	protected String[] address;
 	
 	public User() {}
 	
-	public User(String emailAddress, String firstName, String surName, String password, String[] address) {
+	public User(String emailAddress, String firstname, String surname, String password, String[] address) {
 		this.userID = 0; //TODO: generate random userID
 		this.emailAddress = emailAddress;
-		this.firstName = firstName;
-		this.surName = surName;
+		this.firstname = firstname;
+		this.surname = surname;
 		this.address = address;
 	}
 	
