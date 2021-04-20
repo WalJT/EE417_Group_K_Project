83a5628c688648e64fef60f1c5f9e7765361b8aa
WalJT <waljt@pm.me>
2021-04-20 13:37:46 +0100

    working on logout

--- src/users/LogoutHandler.java
+++ src/users/LogoutHandler.java
@@ -1,8 +1,11 @@
 package users;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,7 +22,7 @@ public class LogoutHandler extends HttpServlet {
      */
     public LogoutHandler() {
         super();
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 
 	/**
@@ -27,14 +30,24 @@ public class LogoutHandler extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		try {
+			// Get all cookies and delete them
+			Cookie[] cookies = request.getCookies();
+		} catch (Exception e) {
+			// TODO graceful error handling
+			e.printStackTrace();
+		} finally {
+			out.close();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// Auto-generated method stub
 		doGet(request, response);
 	}
 
